2ed2698cb43b60592fe5137ee8abcf3987940623
belchus <arenabelu@gmail.com>
2023-01-20 20:56:34 -0300

    se realizaron correcciones

---
+++ express/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+const app = express()
+
+const apiProducts = require('./api/app.js')
+const routeProducts = require('./router/productsRouter.js')
+const cartsRouter = require('./router/cartsRouter.js')
+const PORT = 8080
+const products = new apiProducts('productos.txt')
+
+const server = app.listen(PORT, () =>{
+    console.log(`Servidor HTTP escuchando en puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor, ${error}`))
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/public/views');
+app.use(express.json())
+app.use(express.static(__dirname + '/public'))
+app.use('/api/productos', routeProducts)
+app.use('/api/carrito', cartsRouter)
+
+app.use((req, res) => {
+    res.status(404).json({
+        error: -1,
+        descripcion: `ruta '${req.path}' método '${req.method}' no implementada`
+    })
+})
+
+routeProducts.get('/', async (req, res) => {
+    const productsList = await products.getAll()
+    res.render('index.ejs', {
+        misProd: productsList,
+        productos: productsList.length
+    })
+})
